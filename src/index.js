5c353601e821fd19fbe4dc30acdd044fd4132675
azgron <azgron@gmail.com>
2020-08-01 13:03:13 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -7,15 +7,40 @@ const mongoBackup = require('mongodb-backup');
 const child_process = require('child_process');
 const readdir = require('recursive-readdir');
 const Q = require('q');
+const simpleGit = require('simple-git');
 
-let dir = yargs.argv.dir || process.env.dir;
+
 const uri = yargs.argv.uri || process.env.uri;
 const git = yargs.argv.git || process.env.git;
+let dir = yargs.argv.dir || process.env.dir;
+let cron = process.env.cron || yargs.argv.cron;
+let timezone = yargs.argv.timezone || process.env.timezone || 'Australia/Melbourne';
+let runDf;
 
-if (!dir || !fs.lstatSync(dir).isDirectory()) {
-  throw new Error('Directory not found');
+init();
+
+function init() {
+  dir = path.resolve(__dirname, dir);
+  log('Backing up MongoDB to directory:', dir);
+  if (!uri) {
+    throw new Error('No MongoDB URI provided');
+  }
+  console.log(`Parsed URI: ${uri}`);
+
+  gitInit();
+  run();
+  initCron();
 }
-gitInit(dir, git);
+
+function initCron() {
+  const CronJob = require('cron').CronJob;
+  // Run every hour by default.
+  const cronRange = cron || '00 00 * * * *';
+  const job = new CronJob(cronRange, run,
+    true, /* Start the job right now */
+    timezone
+  );
+}
 
 function gitInit(d, g) {
   log('Git clone...');
@@ -24,37 +49,9 @@ function gitInit(d, g) {
   // const REPO = 'github.com/username/private-repo';
   // const remote = `https://${USER}:${PASS}@${REPO}`;
    
-  const git = require('simple-git');
-  git(dir)
+  simpleGit(dir)
     .clone(git)
     .then(() => console.log('finished'))
-}
-
-dir = path.resolve(__dirname, dir);
-log('Backing up MongoDB to directory:', dir);
-
-if (!uri) {
-  throw new Error('No MongoDB URI provided');
-}
-console.log(`Parsed URI: ${uri}`);
-
-let runDf;
-
-
-// Allow running the processs immediately without scheduling.
-let now = process.env.now || yargs.argv.now;
-let cron = process.env.cron || yargs.argv.cron;
-let timezone = yargs.argv.timezone || process.env.timezone || 'Australia/Melbourne';
-if (now != null) {
-  run();
-} else {
-  const CronJob = require('cron').CronJob;
-  // Run every hour by default.
-  const cronRange = cron || '00 00 * * * *';
-  const job = new CronJob(cronRange, run,
-    true, /* Start the job right now */
-    timezone
-  );
 }
 
 function run() {
@@ -95,8 +92,8 @@ function push() {
     if (err) df.reject(err);
   }
 
-  require('simple-git')(dir)
-    .add('./*', callback)
+  simpleGit(dir)
+    .add(`${dir}/*`, callback)
     .then(function() {
       log('Committing...');
     })
@@ -112,27 +109,15 @@ function push() {
   return df.promise;
 }
 
-// Deletes the existing files in the subdirectores of the given directory.
 function deleteFiles(dir) {
   log('Deleting existing files...');
   const df = Q.defer();
 
   // Exclude hidden directories (e.g. git).
-  const dbDir = path.join(dir, uriInfo.db);
+  const dbDir = path.join(dir, '/urban');
   log('Deleting directory:', dbDir);
 
   return Q.ninvoke(child_process, 'exec', `rm -rf ${dbDir}`, {});
-}
-
-// http://stackoverflow.com/questions/18112204
-function getDirectories(srcpath) {
-  return fs.readdirSync(srcpath).filter(function(file) {
-    return fs.statSync(path.join(srcpath, file)).isDirectory();
-  });
-}
-
-function stripColonPrefix(str) {
-  return str.replace(/^:/, '');
 }
 
 function log() {
